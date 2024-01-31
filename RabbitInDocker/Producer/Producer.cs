4fd0ce2d96ce2f7765f2ee52b4d5766621c51064
Stefan Cronert <cronert@yahoo.com>
2024-01-31 15:13:54 +0100

    Skapat en klass för att skicka meddelanden

---
+++ RabbitInDocker/Producer/Producer.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMQ.Client;
+
+namespace Producer;
+
+public class Producer
+{
+    public void SendMessage(string message)
+    {
+        ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+        IConnection connection = factory.CreateConnection();
+        IModel channel = connection.CreateModel();
+
+        channel.QueueDeclare("letterbox");
+        var body = System.Text.Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish("", "letterbox", null, body);
+    }
+
+}
